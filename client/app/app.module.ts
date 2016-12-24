c4ae2cfeceb8cf6ef25de2af4bb3722155f83171
afreeman123 <freeman.aidan@gmail.com>
2016-12-24 17:40:25 +0000

    Add header to home page

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { ClarityModule } from 'clarity-angular';
+
 import { AppComponent }  from './app.component';
+import { LoginComponent } from './login.component';
+
+import { routing } from './app.routing';
 
 @NgModule({
   imports:      [ BrowserModule,
-                  ClarityModule ],
-  declarations: [ AppComponent ],
+                  ClarityModule,
+                  routing ],
+  declarations: [ AppComponent,
+                  LoginComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
